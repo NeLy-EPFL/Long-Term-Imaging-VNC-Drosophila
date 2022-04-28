ffb77b4f33d3ac5e39c96ec6b0b1e56e796e6133
Jonas Braun <jonas-braun7@web.de>
2022-04-28 19:51:09 +0200

    updated fly data with new base directory for final test

--- Caffeine_feeding_neural_activity_experiments/revision_flies.py
+++ Caffeine_feeding_neural_activity_experiments/revision_flies.py
@@ -2,9 +2,9 @@ import os.path
 from longterm import load
 from longterm import utils
 
-# BASE_DIR = os.path.join(load.NAS2_DIR_LH, "_dataverse", 
-#                         "test_copy_JB")  # Caffeine_feeding_neural_activity_experiments
-BASE_DIR = os.path.join(load.NAS2_DIR_LH)
+BASE_DIR = os.path.join(load.NAS2_DIR_LH, "_dataverse", 
+                        "test_copy_revisions")  # Caffeine_feeding_neural_activity_experiments
+# BASE_DIR = os.path.join(load.NAS2_DIR_LH)
 conditions = [
     "High_Caffeine_Fly1",
     "High_Caffeine_Fly2",
@@ -13,14 +13,14 @@ conditions = [
     "Low_Caffeine_Fly1",
     "Sucrose_Fly_1",
 
-    "Low_Caffeine_SupFly2",
-    "Low_Caffeine_SupFly3",
+    "Low_Caffeine_Fly2",
+    "Low_Caffeine_Fly3",
 
-    "Sucrose_SupFly2",
-    "Sucrose_SupFly3"
+    "Sucrose_Fly2",
+    "Sucrose_Fly3"
 ]
 
-subdirs = [
+recorded_dates = [
     "210722/fly3",  # high caff 1
     "210721/fly3",  # high caff 2
     "210723/fly2",  # high caff 3
@@ -36,25 +36,37 @@ subdirs = [
 ]
 
 omitted_trials = [  
-    # ommit 2/4 pre-feeding trials
-    # ommit one of the feeding trials in case they are 2
     # ommit excess trials in the end
-    [0, 2, 13],
-    [1, 2],
-    # omitt 1 instead of 0 because fly moves slightly more and thus the normalisation will work better
-    [0, 2, 4, 14, 15],  # first feeding trial omitted and last trial missing processing
+    [11],
+    [],
+    [],
     
-    [0, 2],
-    [0, 1],
+    [],
+    [],
     
-    [0, 2],
-    [0, 2],
+    [],
+    [],
     
-    [0, 2],
-    [0, 1, 4]  # first feeding trial omitted
+    [],
+    []
 ]
 
-fly_dirs = [os.path.join(BASE_DIR, subdir) for subdir in subdirs]
+beh_trials_missing = [
+    [],
+    [],
+    [10],
+    
+    [],
+    [],
+    
+    [],
+    [],
+    
+    [3],
+    []
+]
+
+fly_dirs = [os.path.join(BASE_DIR, condition) for condition in conditions]
 
 class Trial():
     def __init__(self, fly, trial_dir, beh_trial_dir=None, sync_trial_dir=None):
@@ -64,14 +76,14 @@ class Trial():
         self.beh_dir = beh_trial_dir if beh_trial_dir is not None else trial_dir
         self.sync_dir = sync_trial_dir if sync_trial_dir is not None else trial_dir
         self.processed_dir = os.path.join(self.dir, load.PROCESSED_FOLDER)
-        self.green_denoised = os.path.join(self.processed_dir, "green_denoised.tif")  # used
-        self.green_raw = os.path.join(self.processed_dir, "green_com_warped.tif")  # used
-        self.red_raw = os.path.join(self.processed_dir, "red_com_warped.tif")  # used
-        self.green_norm = os.path.join(self.processed_dir, "green_denoised_norm.tif")  # used
-        self.dff = os.path.join(self.processed_dir, "dff_denoised.tif")  # used
-        self.name = self.dir.split("/")[-1].replace("_", " ")  # used
-        self.twop_df = os.path.join(self.processed_dir, "twop_df.pkl")  # used TODO: include in processing pipeline
-        self.opflow_df = os.path.join(self.processed_dir, "opflow_df.pkl")  # used TODO: include in processing pipeline
+        self.green_denoised = os.path.join(self.processed_dir, "green_denoised.tif")
+        self.green_raw = os.path.join(self.processed_dir, "green_com_warped.tif")
+        self.red_raw = os.path.join(self.processed_dir, "red_com_warped.tif")
+        self.green_norm = os.path.join(self.processed_dir, "green_denoised_norm.tif")
+        self.dff = os.path.join(self.processed_dir, "dff_denoised.tif")
+        self.name = self.dir.split("/")[-1].replace("_", " ")
+        self.twop_df = os.path.join(self.processed_dir, "twop_df.pkl")
+        self.opflow_df = os.path.join(self.processed_dir, "opflow_df.pkl")
 
 
 class Fly():
@@ -86,23 +98,23 @@ class Fly():
         trial_dirs = load.get_trials_from_fly(self.dir, startswith="cs", exclude="processed")[0]
         self.selected_trials = [i_trial for i_trial in range(len(trial_dirs))
                                 if not i_trial in omitted_trials[self.i_fly]]
-        # range(len(trial_dirs))
+        self.behaviour_trials_missing = beh_trials_missing[self.i_fly]
         self.condition = conditions[self.i_fly]
-        self.paper_condition = self.get_paper_condition(self.condition)  # used
+        self.paper_condition = self.get_paper_condition(self.condition)
         
         self.trials = [Trial(self, trial_dirs[i_trial]) 
                        for i_trial in self.selected_trials]
-        self.fs = 16  # used
-        self.summary_dict = os.path.join(self.processed_dir, "compare_trials.pkl")  # used
+        self.fs = 16
+        self.summary_dict = os.path.join(self.processed_dir, "compare_trials.pkl")
         wave_mask_names = ["mask_top.tif", "mask_left.tif", "mask_right.tif",
                            "mask_bottom.tif", "mask_gf_left.tif", "mask_gf_right.tif"]
-        self.wave_masks = [os.path.join(self.processed_dir, wave_mask) for wave_mask in wave_mask_names]  # used
-        self.trials_mean_dff = os.path.join(self.processed_dir, "trials_mean_dff_revision_099.pkl")  # used
-        self.raw_std = os.path.join(self.processed_dir, "raw_std.tif")  # used
-        self.mask_fine = os.path.join(self.processed_dir, "cc_mask_fiji.tif")  # used
-        self.wave_details = os.path.join(self.processed_dir, "wave_details.pkl")  # used
-        self.rest_maps = os.path.join(self.processed_dir, "rest_maps.pkl")  # used
-        self.correction = os.path.join(self.processed_dir, "illumination_correction.pkl")  # used
+        self.wave_masks = [os.path.join(self.processed_dir, wave_mask) for wave_mask in wave_mask_names]
+        self.trials_mean_dff = os.path.join(self.processed_dir, "trials_mean_dff_revision_099.pkl")
+        self.raw_std = os.path.join(self.processed_dir, "raw_std.tif")
+        self.mask_fine = os.path.join(self.processed_dir, "cc_mask_fiji.tif")
+        self.wave_details = os.path.join(self.processed_dir, "wave_details.pkl")
+        self.rest_maps = os.path.join(self.processed_dir, "rest_maps.pkl")
+        self.correction = os.path.join(self.processed_dir, "illumination_correction.pkl")
 
     @staticmethod
     def get_paper_condition(condition):
